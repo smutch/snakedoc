dca337442d32cf568f194c605620a6c2c1444e84
Simon Mutch <smutch@unimelb.edu.au>
2022-05-24 14:59:48 +1000

    :cop: More tests for linkcode line numbers

--- tests/test_linkcode.py
+++ tests/test_linkcode.py
@@ -2,11 +2,14 @@ import pytest
 from bs4 import BeautifulSoup
 from sphinx.application import Sphinx
 
-# TODO: don't hardcode url in tests
+
+def _parse_rule_link(rule_name: str, soup: BeautifulSoup):
+    return soup.find("dt", id=f"rule-{rule_name}").find("a", class_="reference external").get("href").split("#L")
 
 
 @pytest.mark.sphinx('html', testroot='docs')
 def test_source_links(app: Sphinx):
+    SNAKEFILE_URL = "https://github.com/smutch/snakedoc/blob/master/snakedoc/tests/workflow/Snakefile"
     app.builder.build_all()
     index = app.outdir / "index.html"
     assert index.exists()
@@ -14,6 +17,7 @@ def test_source_links(app: Sphinx):
     with open(index, "r") as fp:
         soup = BeautifulSoup(fp, "html.parser")
 
-    url, lineno = soup.find("dt", id="rule-basic").find("a", class_="reference external").get("href").split("#L")
-    assert url == "https://github.com/smutch/snakedoc/blob/master/snakedoc/tests/workflow/Snakefile"
-    assert int(lineno) == 5
+    for rule_name, expected in (("basic", 5), ("follows_basic", 26), ("also_follows_basic", 38), ("the_end", 50)):
+        url, lineno = _parse_rule_link(rule_name, soup)
+        assert url == SNAKEFILE_URL
+        assert int(lineno) == expected
