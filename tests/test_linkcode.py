2d9e65a13fa7a87f44a541f92663d24cffa8ebff
Simon Mutch <smutch@unimelb.edu.au>
2022-05-27 15:29:01 +1000

    :cop: Don't hardcode expected line numbers is testing

--- tests/test_linkcode.py
+++ tests/test_linkcode.py
@@ -1,6 +1,12 @@
+import re
+from pathlib import Path
+
 import pytest
 from bs4 import BeautifulSoup
 from sphinx.application import Sphinx
+
+SNAKEFILE_PATH = Path(__file__).absolute().parent / "workflow/Snakefile"
+RULE_PATTERN = re.compile(r"^\s*rule\s+(\S+)\s*:\s*$")
 
 
 def _parse_rule_link(rule_name: str, soup: BeautifulSoup):
@@ -17,7 +23,17 @@ def test_source_links(app: Sphinx):
     with open(index, "r") as fp:
         soup = BeautifulSoup(fp, "html.parser")
 
-    for rule_name, expected in (("basic", 5), ("follows_basic", 26), ("also_follows_basic", 38), ("the_end", 50)):
+    snakefile = SNAKEFILE_PATH.read_text('utf-8').splitlines(keepends=False)
+    for rule_name in ("basic", "follows_basic", "also_follows_basic", "the_end"):
         url, lineno = _parse_rule_link(rule_name, soup)
         assert url == SNAKEFILE_URL
-        assert int(lineno) == expected
+
+        expected = -1
+        for ii, line in enumerate(snakefile):
+            match = RULE_PATTERN.search(line)
+            if match and match.group(1) == rule_name:
+                expected = ii + 1
+                break
+        else:
+            raise ValueError(f"Failed to locate rule {rule_name}")
+        assert int(lineno) == expected, f"Line number for rule {rule_name} does not match expectation"
